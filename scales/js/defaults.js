f41aff955cbcb69a2a892fe9ee476300a6c77e7f
Libor Kramolis <l.kramolis@psp.team>
2026-05-11 11:23:19 +0200

    Vyber Vzhled vystrcen vpravo nahoru.

--- scales/js/defaults.js
+++ scales/js/defaults.js
@@ -1,4 +1,4 @@
 export const DEFAULT_SCALE_ID   = 'C-major';
 export const DEFAULT_PROFILE_ID = 'light-large';
-export const APP_VERSION        = '1.0.7';
-export const BUILD_DATE         = '2026-05-11 11:09';
+export const APP_VERSION        = '1.0.8';
+export const BUILD_DATE         = '2026-05-11 11:40';
