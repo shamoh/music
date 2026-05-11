c7069d1ed07b7bbf3a945a7c76f7adb4eca132c1
Libor Kramolis <l.kramolis@psp.team>
2026-05-11 19:26:22 +0200

    Layout

--- scales/js/defaults.js
+++ scales/js/defaults.js
@@ -1,4 +1,4 @@
 export const DEFAULT_SCALE_ID   = 'C-major';
 export const DEFAULT_PROFILE_ID = 'light-large';
-export const APP_VERSION        = '1.0.21';
-export const BUILD_DATE         = '2026-05-11 18:42';
+export const APP_VERSION        = '1.0.22';
+export const BUILD_DATE         = '2026-05-11 19:24';
