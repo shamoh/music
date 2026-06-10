47ca13667b021794ed66b10565ccb26562a02957
Libor Kramolis <l.kramolis@psp.team>
2026-06-10 09:17:09 +0200

    1.0.45 - estetika v kole

--- docs/js/defaults.js
+++ docs/js/defaults.js
@@ -1,4 +1,4 @@
 export const DEFAULT_SCALE_ID   = 'C-major';
 export const DEFAULT_PROFILE_ID = 'light-large';
-export const APP_VERSION        = '1.0.44';
-export const BUILD_DATE         = '2026-06-09 22:28';
+export const APP_VERSION        = '1.0.45';
+export const BUILD_DATE         = '2026-06-10 08:30';
